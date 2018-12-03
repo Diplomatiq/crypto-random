055afca53355c6ffa453ef665b1755ee7d037ffd
Soma Lucz <soma.lucz@diplomatiq.org>
2018-12-03 23:33:24 +0100

    [#1] Supporting Node.js

--- src/entropyProvider.ts
+++ src/entropyProvider.ts
@@ -1,36 +1,3 @@
-export class EntropyProvider {
-    /**
-     * The crypto implementation. The window.crypto object is used.
-     */
-    private static readonly cryptoImpl: Crypto = window.crypto;
-
-    /**
-     * According to the standard, there is a 2 ** 16 bytes quota for requesting entropy.
-     */
-    private static readonly ENTROPY_QUOTA_BYTES = 65536;
-
-    /**
-     * Puts random values into the given @param typedArray parameter.
-     * If the array's length is greater than the general @member ENTROPY_QUOTA_BYTES,
-     * it is divided into chunks, and filled chunk-by-chunk.
-     */
-    public static async getRandomValues<
-        T extends Uint8Array | Uint16Array | Uint32Array | Int8Array | Int16Array | Int32Array
-    >(array: T): Promise<void> {
-        if (array.byteLength <= this.ENTROPY_QUOTA_BYTES) {
-            this.cryptoImpl.getRandomValues(array);
-            return;
-        }
-
-        let remainingBytes = array.byteLength;
-        while (remainingBytes > 0) {
-            const availableEntropyBytes = Math.min(remainingBytes, this.ENTROPY_QUOTA_BYTES);
-            const chunkStart = array.byteLength - remainingBytes;
-            const chunkLength = availableEntropyBytes / array.BYTES_PER_ELEMENT;
-            const chunkEnd = chunkStart + chunkLength;
-            const chunkToFill = array.subarray(chunkStart, chunkEnd);
-            this.cryptoImpl.getRandomValues(chunkToFill);
-            remainingBytes -= availableEntropyBytes;
-        }
-    }
+export interface EntropyProvider {
+    getRandomValues<T extends Uint8Array | Uint16Array | Uint32Array>(array: T): Promise<T>;
 }
---
+++ src/environmentDetectingEntropyProvider.ts
@@ -0,0 +1,97 @@
+export class EnvironmentDetectingEntropyProvider {
+    /**
+     * The crypto implementation used in the browser. The window.crypto object is used.
+     */
+    private readonly browserCrypto: Crypto | undefined;
+
+    /**
+     * The crypto implementation used in Node.js environments. This will be provided by the "crypto" module of Node.
+     */
+    private readonly nodeCrypto: any;
+
+    /**
+     * According to the Web Crypto standard, there is a 2 ** 16 bytes quota for requesting entropy.
+     */
+    private static readonly BROWSER_ENTROPY_QUOTA_BYTES = 65536;
+
+    /**
+     * The environment in which the package is run.
+     */
+    private readonly ENVIRONMENT: 'browser' | 'node';
+
+    constructor() {
+        if (typeof window !== 'undefined' && typeof window.document !== 'undefined') {
+            this.ENVIRONMENT = 'browser';
+            if (typeof window.crypto !== 'undefined' && typeof window.crypto.getRandomValues !== 'undefined') {
+                this.browserCrypto = window.crypto;
+            } else {
+                throw new Error('window.crypto.getRandomValues not available');
+            }
+        } else if (typeof process !== 'undefined' && process.versions && process.versions.node) {
+            this.ENVIRONMENT = 'node';
+            try {
+                this.nodeCrypto = require('crypto');
+            } catch (e) {
+                throw new Error('NodeJS.crypto not available');
+            }
+        } else {
+            throw new Error('Unexpected environment: neither browser nor node');
+        }
+    }
+
+    /**
+     * Puts random values into the given @param array in a browser environment.
+     * If the array's length is greater than the general @member BROWSER_ENTROPY_QUOTA_BYTES,
+     * it is divided into chunks, and filled chunk-by-chunk.
+     *
+     * Can be only called, if @member ENVIRONMENT === 'browser'.
+     */
+    private async getRandomValuesBrowser<T extends Uint8Array | Uint16Array | Uint32Array>(array: T): Promise<T> {
+        if (array.byteLength <= EnvironmentDetectingEntropyProvider.BROWSER_ENTROPY_QUOTA_BYTES) {
+            return this.browserCrypto!.getRandomValues(array);
+        }
+
+        let remainingBytes = array.byteLength;
+        while (remainingBytes > 0) {
+            const availableEntropyBytes = Math.min(
+                remainingBytes,
+                EnvironmentDetectingEntropyProvider.BROWSER_ENTROPY_QUOTA_BYTES,
+            );
+            const chunkStart = array.byteLength - remainingBytes;
+            const chunkLength = availableEntropyBytes / array.BYTES_PER_ELEMENT;
+            const chunkEnd = chunkStart + chunkLength;
+            const chunkToFill = array.subarray(chunkStart, chunkEnd);
+            this.browserCrypto!.getRandomValues(chunkToFill);
+            remainingBytes -= availableEntropyBytes;
+        }
+        return array;
+    }
+
+    /**
+     * Puts random values into the given @param array in a Node.js environment.
+     * Can be only called, if @member ENVIRONMENT === 'node'.
+     */
+    private async getRandomValuesNode<T extends Uint8Array | Uint16Array | Uint32Array>(array: T): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            this.nodeCrypto.randomFill(array, (error: Error | null, array: T) => {
+                if (error) {
+                    reject(error);
+                }
+
+                resolve(array);
+            });
+        });
+    }
+
+    /**
+     * Puts random values into the given @param array parameter.
+     */
+    public async getRandomValues<T extends Uint8Array | Uint16Array | Uint32Array>(array: T): Promise<T> {
+        switch (this.ENVIRONMENT) {
+            case 'browser':
+                return await this.getRandomValuesBrowser(array);
+            case 'node':
+                return await this.getRandomValuesNode(array);
+        }
+    }
+}
